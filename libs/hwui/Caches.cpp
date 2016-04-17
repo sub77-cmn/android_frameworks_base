5b646bfa847f51c0ab31dbcaeff5e80d4bb0b4de
ferhung-mtk <ferhung27@gmail.com>
2016-04-17 20:49:46 +0800

    MediaTek: the improving & fixing of hwui crashing.
    Signed-off-by: sub77 <sub77@ymail.com>

--- libs/hwui/Caches.cpp
+++ libs/hwui/Caches.cpp
@@ -80,16 +80,21 @@ bool Caches::init() {
 }
 
 void Caches::initExtensions() {
+#ifndef MTK_HARDWARE
     if (mExtensions.hasDebugMarker()) {
         eventMark = glInsertEventMarkerEXT;
-
         startMark = glPushGroupMarkerEXT;
         endMark = glPopGroupMarkerEXT;
     } else {
-        eventMark = eventMarkNull;
-        startMark = startMarkNull;
-        endMark = endMarkNull;
+         eventMark = eventMarkNull;
+         startMark = startMarkNull;
+         endMark = endMarkNull;
     }
+#else
+         eventMark = eventMarkNull;
+         startMark = startMarkNull;
+         endMark = endMarkNull;
+#endif
 }
 
 void Caches::initConstraints() {
